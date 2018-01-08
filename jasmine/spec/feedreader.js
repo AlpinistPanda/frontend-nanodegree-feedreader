81fb6250767b403efdedd62d88da0e60d13a8154
ozgunbalaban <ozgunbalaban@yahoo.com>
2018-01-08 16:28:24 +0800

    has name defined implemented

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -44,6 +44,13 @@ $(function() {
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+
+     it('has name defined', function() {
+       for (let feed of allFeeds) {
+         expect(feed.name).toBeDefined();
+         expect(feed.name).not.toBe('');
+     }
+   });
     });
 
 
